424a283c5588fc32f370f3cdbb3ee02b3cc00054
Adri Bagas <abagaswitjaksono@gmail.com>
2026-01-04 22:21:07 +0700

    feat: a very simple login function yikes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,20 +1,61 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+// DISCLAIMER: UNTUK PARA PESERTA BOOTCAMP JANGAN PERNAH PAKE CARA INI BUAT APLIKASI KALIAN,
+// CUMA BUAT LATIHAN DOANG!!!
+
 /* POST login. */
-router.post('/login', function(req, res, next) {
-  res.status(501).json({
-    message: "Route belum di implementasi!",
+router.post("/login", function (req, res, next) {
+  const { password } = req.body;
+  const SECRET = process.env.SUPER_SECRET_PASSPRASE_TEHEE;
+
+  if (!SECRET) {
+    res.status(501).json({
+      message: "Secret password belum dibuat!",
+      success: false,
+      data: null,
+    });
+  }
+
+  if (password === SECRET) {
+    const SECRET_KEY = process.env.SUPER_SECRET_KEY;
+
+    if (!SECRET_KEY) {
+      res.status(501).json({
+        message: "Secret key belum dibuat!",
+        success: false,
+        data: null,
+      });
+    }
+
+    res.cookie("x-a-very-secret-key", SECRET_KEY, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+      sameSite: "lax",
+      path: "/",
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Anda berhasil login!",
+      data: null,
+    });
+  }
+
+  res.status(401).json({
+    message: "Password salah!",
     success: false,
     data: null,
   });
 });
 
 /* POST logout. */
-router.post('/logout', function(req, res, next) {
-  res.status(501).json({
-    message: "Route belum di implementasi!",
-    success: false,
+router.post("/logout", function (req, res, next) {
+  res.clearCookie("x-a-very-secret-key");
+
+  return res.status(200).json({
+    success: true,
+    message: "Anda berhasil Logout!",
     data: null,
   });
 });
